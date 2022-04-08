5aa0bfbb1375c22bb7538f2100923308d47e2574
Daniel Silva de Sousa <diielsilvasousa@gmail.com>
2022-04-08 11:43:24 -0300

    Fixing bug to auth user

--- src/main/java/com/olix/stock_system/api/controller/AuthController.java
+++ src/main/java/com/olix/stock_system/api/controller/AuthController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.olix.stock_system.api.helper.EncoderHelper;
 import com.olix.stock_system.api.representation.output.ShowUser;
 import com.olix.stock_system.common.mapper.ModelMapper;
 import com.olix.stock_system.domain.model.CustomUser;
@@ -17,15 +18,18 @@ import com.olix.stock_system.domain.service.AuthService;
 public class AuthController {
 	private AuthService authService;
 	private ModelMapper modelMapper;
+	private EncoderHelper encoderHelper;
 
-	public AuthController(AuthService authService, ModelMapper modelMapper) {
+	public AuthController(AuthService authService, ModelMapper modelMapper, EncoderHelper encoderHelper) {
 		this.authService = authService;
 		this.modelMapper = modelMapper;
+		this.encoderHelper = encoderHelper;
 	}
 
 	@PostMapping
 	public ResponseEntity<ShowUser> authenticate(@RequestHeader(value = "Authorization") String encodedHeader) {
-		CustomUser customUser = this.authService.findByUsername(encodedHeader);
+		String username = this.encoderHelper.decodeAuthRequestHeader(encodedHeader);
+		CustomUser customUser = this.authService.findByUsername(username);
 		ShowUser showUser = this.modelMapper.getCustomUserMapper().fromCustomUserToShowUser(customUser);
 		return new ResponseEntity<ShowUser>(showUser, HttpStatus.OK);
 	}
